8dbee8e64e3a775e73ee8f1e106a7b8bae6d1c01
Your Name <you@example.com>
2024-11-06 20:14:31 +0200

    take input and validate data

--- lib/widgets/cutom_text_field.dart
+++ lib/widgets/cutom_text_field.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constnts.dart';
 
 class CutomTextField extends StatelessWidget {
-  const CutomTextField({super.key, required this.hint, this.maxlines = 1});
+  const CutomTextField(
+      {super.key, required this.hint, this.maxlines = 1, this.onSaved});
   final String hint;
   final int maxlines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field Is Required';
+        } else {
+          return null;
+        }
+      },
       maxLines: maxlines,
       cursorColor: kPrimaryColor,
       decoration: InputDecoration(
